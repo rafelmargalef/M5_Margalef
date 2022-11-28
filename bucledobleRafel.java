ae2ceab3d6c3f5c53f4cf768e648883c1681c40f
rafelmargalef <rafelmargalef@iesmontsia.org>
2022-11-28 09:14:51 +0100

    Programa M3

---
+++ bucledobleRafel.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class bucledobleRafel {
+  public static void main(String[] args) {
+    int j=0;
+    int x=0;
+    while (j<5) {
+      while (x<3) {
+        
+        System.out.print("-");
+        x++;
+      }
+      System.out.println();
+      j++;
+      x=0;
+    }
+
+    System.out.println();
+
+    j=0;
+    x=0;
+    while (j<3) {
+      while (x<5) {
+        
+        System.out.print("-");
+        x++;
+      }
+      System.out.println();
+      j++;
+      x=0;
+    }
+
+    
+
+  }
+}
